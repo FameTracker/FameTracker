e15a9d37a77a29851b0e979eecd9f449608537da
Assynu <66019090+ASSYNU@users.noreply.github.com>
2022-03-27 15:58:31 +0200

    Added Videos Tab

---
+++ FameTreacker/FameTreacker/Features/Videos/VideosView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FameTreacker.Features.Videos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VideosView : ContentPage
+    {
+        public VideosView()
+        {
+            InitializeComponent();
+        }
+    }
+}
